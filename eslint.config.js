6f4db134302904822df1dc89dae54f72582542ba
tonyap92 <114396404+tonyap92@users.noreply.github.com>
2024-06-27 21:01:48 +0300

    Put a project and added dependencies.

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+const pluginVue = require('eslint-plugin-vue');
+
+module.exports = [
+  // add more generic rulesets here, such as:
+  // js.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+    },
+  },
+];
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <div>
+    <keep-alive>
+      <router-view />
+    </keep-alive>
+  </div>
+</template>
+
+<style scoped>
+</style>
